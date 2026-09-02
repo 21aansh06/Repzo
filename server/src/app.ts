5f50316680fef2ade3adb465afad3571984b4c90
Aansh Malhotra <aanshmalhotra49@gmail.com>
2026-09-03 00:02:06 +0530

    inngest setup ready

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,9 +1,13 @@
 import express from "express";
-
+import { inngest, functions } from "./inngest/index.js";
+import { serve } from "inngest/express";
 
 const app = express()
 
 app.use(express.json());
+app.use(express.json());
+app.use("/api/inngest", serve({ client: inngest, functions }));
+
 app.get("/", (req,res)=>{
     res.send("hello!")
 })
